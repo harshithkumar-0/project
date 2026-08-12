8dc8ac81eb9672420c94ca9c23d27aa472403e88
Harshith kumar <harshithkumar701935@gmail.com>
2026-08-12 15:45:41 +0530

    4 updated

--- app.js
+++ app.js
@@ -393,5 +393,9 @@ function sendWidgetMessage(text) {
   document.getElementById('chatbot-widget-input').value = text;
   document.querySelector('.chatbot-input-form').dispatchEvent(new Event('submit'));
 }
-
+push //
+\\
+';
+\\
+'
 
